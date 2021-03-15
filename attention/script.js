c53696675771f23630eccc1bb7fd9c79219bbd3b
segyong56 <80687195+segyong56@users.noreply.github.com>
2021-03-16 02:15:38 +0900

    Add files via upload

---
+++ attention/script.js
@@ -0,0 +1,21 @@
+
+
+document.getElementById("btn1").onclick = function () {
+   location.href = "attention-number/index.html";
+};
+
+document.getElementById("btn2").onclick = function () {
+    location.href = "attention-numberText/index.html";
+ };
+
+ document.getElementById("btn3").onclick = function () {
+    location.href = "attention-findRandom/index.html";
+ };
+
+ document.getElementById("btn4").onclick = function () {
+    location.href = "attention-pressArrow/index.html";
+ };
+
+ document.getElementById("btn5").onclick = function () {
+    location.href = "attention-colorText/index.html";
+ };
